6ea6c131f24eb868456413f83e3e4579d4e5f7d8
shila-msg <sheilamesgarzadeh80@gmail.com>
2025-10-23 15:09:13 +0100

    completed activity 4
    an alert comes up if modules are clicked

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,21 +1,27 @@
-import { StyleSheet, Text} from 'react-native';
-import Screen from '../layout/Screen';
- const ModuleListScreen = () => {
+import { StyleSheet } from "react-native";
+import Screen from "../layout/Screen";
+import ModuleList from "../entity/modules/ModuleList.js";
+
+import initialModules from "../../data/modules.js";
+
+const ModuleListScreen = () => {
   // Installations....
+  const modules = initialModules;
+
   // State....
   // Handlers...
+  const handleSelect = (module) => alert(`Item ${module.ModuleCode} selected`);
+
   // View...
   return (
     <Screen>
-
-      <Text>List</Text>
-
-
+      <ModuleList modules={modules} onSelect={handleSelect} />
     </Screen>
   );
-}
+};
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  contaoiner: {},
+});
 
 export default ModuleListScreen;
-
