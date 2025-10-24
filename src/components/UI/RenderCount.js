e4a64a2a93189abff812649b23efe9f52f7b06ca
shila-msg <sheilamesgarzadeh80@gmail.com>
2025-10-24 13:05:37 +0100

    completed activity 5
    number of modules are shown. removed modules pressed on

---
+++ src/components/UI/RenderCount.js
@@ -0,0 +1,30 @@
+import { useRef } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const RenderCount = ({
+  backgroundColor = "dodgerblue",
+  fontColor = "white",
+}) => {
+  //Initialiasations...
+  //State...
+
+  const renderCount = useRef(0);
+  renderCount.current += 1;
+
+  //Handlers...
+  //View...
+
+  return (
+    <Text style={[styles.count, { backgroundColor, color: fontColor }]}>
+      Number of renders : {renderCount.current}
+    </Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  count: {
+    padding: 5,
+  },
+});
+
+export default RenderCount;
