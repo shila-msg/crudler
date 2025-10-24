38889a31ac89eaee277bc645f35c274d800550ae
shila-msg <sheilamesgarzadeh80@gmail.com>
2025-10-24 13:09:48 +0100

    completed activity 5 (#3)
    number of modules are shown. removed modules pressed on

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,21 +1,25 @@
+import { useState } from "react";
 import { StyleSheet } from "react-native";
 import Screen from "../layout/Screen";
 import ModuleList from "../entity/modules/ModuleList.js";
+import RenderCount from "../UI/RenderCount.js";
 
 import initialModules from "../../data/modules.js";
 
 const ModuleListScreen = () => {
   // Installations....
-  const modules = initialModules;
-
   // State....
+  const [modules, setModules] = useState(initialModules);
+
   // Handlers...
-  const handleSelect = (module) => alert(`Item ${module.ModuleCode} selected`);
+  const handleDelete = (module) =>
+    setModules(modules.filter((item) => item.ModuleID !== module.ModuleID));
 
   // View...
   return (
     <Screen>
-      <ModuleList modules={modules} onSelect={handleSelect} />
+      <RenderCount />
+      <ModuleList modules={modules} onSelect={handleDelete} />
     </Screen>
   );
 };
