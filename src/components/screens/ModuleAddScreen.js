6acdd0fd9fa004affbba0b7ceabb7acbd11cac7f
shila-msg <sheilamesgarzadeh80@gmail.com>
2025-11-27 14:12:22 +0000

    activity 9 completed
    add and cancel buttons added to add screen

--- src/components/screens/ModuleAddScreen.js
+++ src/components/screens/ModuleAddScreen.js
@@ -1,21 +1,39 @@
-import { StyleSheet, Text} from 'react-native';
-import Screen from '../layout/Screen';
- const ModuleAddScreen = () => {
+import { StyleSheet, Text } from "react-native";
+import Screen from "../layout/Screen";
+import Icons from "../UI/Icons.js";
+import { Button, ButtonTray } from "../UI/Button.js";
+
+const defaultModule = {
+  ModuleID: Math.floor(10000 + Math.random() * 9000),
+  ModuleCode: "CI6330",
+  ModuleName: "Mobile Application Development",
+  moduleLevel: "6",
+  ModuleLeaderID: "1",
+  ModuleLeader: "Graeme JONES",
+  ModuleImage:
+    "https://images.freeimages.com/images/small-previews/cf5/cellphone-1313194.jpg",
+};
+
+const ModuleAddScreen = ({ navigation, route }) => {
   // Installations....
+  const { onAdd } = route.params;
+
   // State....
   // Handlers...
+  const handleAdd = () => onAdd(defaultModule);
+  const handleCancel = navigation.goBack;
   // View...
   return (
     <Screen>
-
       <Text>Add</Text>
-
-
+      <ButtonTray>
+        <Button label="Add" icon={<Icons.Add />} onClick={handleAdd} />
+        <Button label="Cancel" icon={<Icons.Add />} onClick={handleCancel} />
+      </ButtonTray>
     </Screen>
   );
-}
+};
 
 const styles = StyleSheet.create({});
 
 export default ModuleAddScreen;
-
