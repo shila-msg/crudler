48148fabf7ec1d4aad14377523a007cb476c3e91
shila-msg <sheilamesgarzadeh80@gmail.com>
2025-11-27 15:02:25 +0000

    completed activity 9 (#8)
    new module can be added successfully with all the details

--- src/components/UI/Icons.js
+++ src/components/UI/Icons.js
@@ -2,12 +2,16 @@ import { MaterialIcons } from "@expo/vector-icons";
 
 const Icons = {};
 const Add = () => <MaterialIcons name="add" size={16} />;
+const Close = () => <MaterialIcons name="close" size={16} />;
 const Delete = () => <MaterialIcons name="delete" size={16} />;
 const Edit = () => <MaterialIcons name="edit" size={16} />;
+const Submit = () => <MaterialIcons name="check" size={16} />;
 
 //compose
 Icons.Add = Add;
 Icons.Delete = Delete;
 Icons.Edit = Edit;
+Icons.Close = Close;
+Icons.Submit = Submit;
 
 export default Icons;
