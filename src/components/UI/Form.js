0fbe37f9e6465f0ede493ccba9ca8d57837a0fc5
shila-msg <sheilamesgarzadeh80@gmail.com>
2025-12-04 13:41:35 +0000

    completed activity 11 (#9)
    installed dropdown select list and used instead of picker to fix ios compatibality

---
+++ src/components/UI/Form.js
@@ -0,0 +1,126 @@
+import {
+  KeyboardAvoidingView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { SelectList } from "react-native-dropdown-select-list";
+import { Button, ButtonTray } from "./Button.js";
+import Icons from "./Icons.js";
+
+const Form = ({ children, onSubmit, onCancel, submitLabel, submitIcon }) => {
+  //Initializations...
+  //State...
+  //Handlers...
+  //View...
+  return (
+    <KeyboardAvoidingView style={styles.formContainer}>
+      <ScrollView contentContainerStyle={styles.formItems}>
+        {children}
+      </ScrollView>
+
+      <ButtonTray>
+        <Button label={submitLabel} icon={submitIcon} onClick={onSubmit} />
+        <Button label="Cancel" icon={<Icons.Close />} onClick={onCancel} />
+      </ButtonTray>
+    </KeyboardAvoidingView>
+  );
+};
+
+const InputText = ({ label, value, onChange }) => {
+  //Initializations...
+  //State...
+  //Handlers...
+  //View...
+  return (
+    <View style={styles.item}>
+      <Text style={styles.itemLabel}>{label}</Text>
+      <TextInput
+        value={value}
+        onChangeText={onChange}
+        style={styles.itemTextInput}
+      />
+    </View>
+  );
+};
+
+const InputSelect = ({ label, prompt, options, value, onChange }) => {
+  //Initializations...
+  //State...
+  //Handlers...
+  //View...
+  const selectListData = options.map((option) => ({
+    key: option.value,
+
+    value: option.label,
+  }));
+
+  return (
+    <View style={styles.item}>
+      <Text style={styles.itemLabel}>{label}</Text>
+
+      <SelectList
+        setSelected={onChange}
+        data={selectListData}
+        placeholder={prompt}
+        defaultOption={selectListData.find((item) => item.key === value)}
+        boxStyles={styles.selectListBoxStyle}
+        dropdownStyles={styles.selectListDropdownStyle}
+      />
+    </View>
+  );
+};
+
+// Compose components
+Form.InputText = InputText;
+Form.InputSelect = InputSelect;
+
+//Styles
+
+const styles = StyleSheet.create({
+  formContainer: {
+    gap: 10,
+  },
+
+  formItems: {
+    gap: 5,
+  },
+
+  itemLabel: {
+    color: "grey",
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  itemTextInput: {
+    height: 50,
+    paddingLeft: 10,
+    fontSize: 16,
+    backgroundColor: "white",
+    borderRadius: 7,
+    borderWidth: 1,
+    borderColor: "lightgrey",
+  },
+  selectListBoxStyle: {
+    height: 50,
+
+    backgroundColor: "whitesmoke",
+
+    borderRadius: 7,
+
+    borderWidth: 1,
+
+    borderColor: "lightgrey",
+
+    paddingLeft: 10,
+
+    paddingTop: 15,
+  },
+
+  selectListDropdownStyle: {
+    borderColor: "lightgrey",
+  },
+});
+
+export default Form;
--- src/components/screens/ModuleAddScreen.js
+++ src/components/screens/ModuleAddScreen.js
@@ -1,106 +1,23 @@
-import { useState } from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { StyleSheet } from "react-native";
 import Screen from "../layout/Screen";
-import Icons from "../UI/Icons.js";
-import { Button, ButtonTray } from "../UI/Button.js";
-
-const defaultModule = {
-  ModuleID: null,
-  ModuleCode: null,
-  ModuleName: null,
-  ModuleLevel: null,
-  ModuleLeaderID: null,
-  ModuleLeaderName: null,
-  ModuleImage: null,
-};
+import ModuleForm from "../entity/modules/ModuleForm.js";
 
 const ModuleAddScreen = ({ navigation, route }) => {
   // Installations....
   const { onAdd } = route.params;
-  defaultModule.ModuleID = Math.floor(10000 + Math.random() * 9000);
-  defaultModule.ModuleImage =
-    "https://images.freeimages.com/images/small-previews/cf5/cellphone-1313194.jpg";
 
   // State....
-  const [module, setModule] = useState(defaultModule);
-
   // Handlers...
-  const handleAdd = () => onAdd(module);
   const handleCancel = navigation.goBack;
-  const handleChange = (field, value) =>
-    setModule({ ...module, [field]: value });
 
   // View...
   return (
     <Screen>
-      <View style={styles.item}>
-        <Text style={styles.itemLabel}>Module code</Text>
-        <TextInput
-          value={module.ModuleCode}
-          onChangeText={(value) => handleChange("ModuleCode", value)}
-          style={styles.itemTextInput}
-        />
-      </View>
-
-      <View style={styles.item}>
-        <Text style={styles.itemLabel}>Module name</Text>
-        <TextInput
-          value={module.ModuleName}
-          onChangeText={(value) => handleChange("ModuleName", value)}
-          style={styles.itemTextInput}
-        />
-      </View>
-
-      <View style={styles.item}>
-        <Text style={styles.itemLabel}>Module level</Text>
-        <TextInput
-          value={module.ModuleLevel}
-          onChangeText={(value) => handleChange("ModuleLevel", value)}
-          style={styles.itemTextInput}
-        />
-      </View>
-
-      <View style={styles.item}>
-        <Text style={styles.itemLabel}>Module leader </Text>
-        <TextInput
-          value={module.ModuleLeaderName}
-          onChangeText={(value) => handleChange("ModuleLeaderName", value)}
-          style={styles.itemTextInput}
-        />
-      </View>
-
-      <View style={styles.item}>
-        <Text style={styles.itemLabel}>Module image URL</Text>
-        <TextInput
-          value={module.ModuleImage}
-          onChangeText={(value) => handleChange("ModuleImage", value)}
-          style={styles.itemTextInput}
-        />
-      </View>
-
-      <ButtonTray>
-        <Button label="Add" icon={<Icons.Add />} onClick={handleAdd} />
-        <Button label="Cancel" icon={<Icons.Close />} onClick={handleCancel} />
-      </ButtonTray>
+      <ModuleForm onSubmit={onAdd} onCancel={handleCancel} />
     </Screen>
   );
 };
 
-const styles = StyleSheet.create({
-  itemLabel: {
-    color: "grey",
-    fontSize: 16,
-    marginBottom: 5,
-  },
-  itemTextInput: {
-    height: 50,
-    paddingLeft: 10,
-    fontSize: 16,
-    backgroundColor: "white",
-    borderRadius: 7,
-    borderWidth: 1,
-    borderColor: "lightgrey",
-  },
-});
+const styles = StyleSheet.create({});
 
 export default ModuleAddScreen;
