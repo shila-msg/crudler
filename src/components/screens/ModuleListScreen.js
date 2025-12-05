e462198c6fd2db05a150435f2c6b05f61c33153d
shila-msg <sheilamesgarzadeh80@gmail.com>
2025-12-05 12:11:59 +0000

    completed activity 14
    created a hook

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,6 +1,5 @@
-import { useState, useEffect } from "react";
 import { LogBox, StyleSheet, Text } from "react-native";
-import API from "../API/API.js";
+import useLoad from "../API/useLoad.js";
 import Screen from "../layout/Screen";
 import Icons from "../UI/Icons.js";
 import { Button, ButtonTray } from "../UI/Button.js";
@@ -17,18 +16,8 @@ const ModuleListScreen = ({ navigation }) => {
   const modulesEndpoint = "https://softwarehub.uk/unibase/api/modules";
 
   // State....
-  const [modules, setModules] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const loadModules = async (endpoint) => {
-    const response = await API.get(endpoint);
-    setIsLoading(false);
-    if (response.isSuccess) setModules(response.result);
-  };
-
-  useEffect(() => {
-    loadModules(modulesEndpoint);
-  }, []);
+  const [modules, setModules, isLoading, loadModules] =
+    useLoad(modulesEndpoint);
 
   // Handlers...
   const handleAdd = (newModule) => setModules([...modules, newModule]);
